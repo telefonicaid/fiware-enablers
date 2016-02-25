a85d8d57cb537dae6e4b36d74f2ff2d15acc2e14
Henar Muñoz Frutos <henar.munozfrutos@telefonica.com>
2016-02-25 08:35:34 +0100

    Updating tests

--- package-generator/tests/acceptance/test_deploy_packages.py
+++ package-generator/tests/acceptance/test_deploy_packages.py
@@ -96,6 +96,8 @@ class DeployPackagesTest(core.MuranoTestsCore, unittest.TestCase):
         self.murano_apps_folder = core.CONF.murano.murano_apps_folder
         self.murano_package = package_str
 
+        self.murano_package = "iotagent"
+
         package = self.get_package(self.murano_package)
         if package:
             self.delete_package(package)
@@ -107,15 +109,19 @@ class DeployPackagesTest(core.MuranoTestsCore, unittest.TestCase):
                                             "tags": ["tag"]})
 
         images = ["base_ubuntu_14.04", "base_centos_7"]
+        atts = {}
         tags = uploaded_package.tags
         for tag in tags:
             if "images" in tag:
                 tag = tag[7:len(tag)]
                 images = tag.split(';')
+            if "attributes" in tag:
+                tag = tag[11:len(tag)]
+                atts = tag.split(';')
 
         for image in images:
             self.linux = image
-            self._test_deploy(self.murano_package, package_id, 22)
+            self._test_deploy(self.murano_package, package_id, 22, atts)
             self.purge_environments()
 
     def delete_package(cls, package):
