2a38096bce3f27826fe06e40e401fc5c5a5a1a68
visitor <henar.munozfrutos@telefonica.com>
2016-10-17 12:54:41 +0200

    Work towards comments

--- package-generator/other.sh
+++ package-generator/other.sh
@@ -1,2 +1,6 @@
-python package-generator.py -u ${USERNAME} -g False -t ${TENANT_ID} -p ${PASSWORD}
-python migrate-templates.py -u ${USERNAME} -t ${TENANT_ID} -p ${PASSWORD}
+[ -z "$OS_USERNAME" ] && echo "Need variable OS_USERNAME" && exit 1;
+[ -z "$OS_TENANT_ID" ] && echo "Need variable OS_TENANT_ID" && exit 1;
+[ -z "$OS_PASSWORD" ] && echo "Need variable OS_PASSWORD" && exit 1;
+
+python package-generator.py -u ${OS_USERNAME} -g False -t ${OS_TENANT_ID} -p ${OS_PASSWORD}
+python migrate-templates.py -u ${OS_USERNAME} -t ${OS_TENANT_ID} -p ${OS_PASSWORD}
