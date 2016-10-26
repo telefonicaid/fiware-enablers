335988bc7aed98e7fc94d59cfa1d1757bb448ecc
visitor <henar.munozfrutos@telefonica.com>
2016-10-26 11:40:25 +0200

    update scripts

--- package-generator/start.sh
+++ package-generator/start.sh
@@ -1,6 +1,6 @@
 python setup.py install
 sed -i -e "s/XXX/${PASSWORD}/" /opt/fiware-enablers/package-generator/tests/acceptance/config.conf
-sed -i -e "s/*log*//" /opt/fiware-enablers/package-generator/package-generator.py
+sed -i -e "s/*log*//" /opt/fiware-enablers/package-generator/packagegenerator/package-generator.py
 /opt/fiware-enablers/package-generator/other.sh
 nosetests --with-xunit --xunit-file /opt/test.xml  tests 
 
